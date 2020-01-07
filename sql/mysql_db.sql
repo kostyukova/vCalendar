3def60d8893afae55a0e88c89574caec188e4a1e
Elena Kostyukova <Elena.Kostyukova@ingrammicro.com>
2020-01-07 22:17:48 +0700

    Swagger API definition draft

--- sql/mysql_db.sql
+++ sql/mysql_db.sql
@@ -6,8 +6,8 @@ CREATE OR REPLACE TABLE `employee` (
   `full_name` varchar(255) NOT NULL,
   `position` varchar(20) NOT NULL,
   `specialization` VARCHAR(10) DEFAULT '',
-  `team_number` TINYINT(1) DEFAULT 1,
-  `expert` TINYINT(1) DEFAULT 0,
+  `team_number` TINYINT(1) NOT NULL DEFAULT 1,
+  `expert` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
@@ -32,10 +32,24 @@ CREATE OR REPLACE TABLE `employee_leave_days` (
   CONSTRAINT `FK_employee_leave_days` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
+CREATE OR REPLACE TABLE `user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `roles` SET('read:users','write:users','read:employees', 'write:employees') NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+
 INSERT INTO `employee`(`full_name`, `position`, `specialization`, `team_number`, `expert`)
     VALUES ('Иванов', 'junior engineer', '', 1, 0);
 INSERT INTO `employee_total_days`(`employee_id`, `total_days`, `year`)
     VALUES (1, 28, 2020);
 INSERT INTO `employee_leave_days`(`employee_id`, `leave_days`, `start_date`, `end_date`)
     VALUES(1, 5, '2020-01-06', '2020-01-12');
+
+INSERT INTO `user`(`username`, `password`, `email`, `roles`)
+    VALUES('admin', 'adminPass', 'admin@mail.com', 'read:users,write:users');
+INSERT INTO `user`(`username`, `password`, `email`, `roles`)
+    VALUES('user', 'userPass', 'user@mail.com', 'read:employees,write:employees');
 
