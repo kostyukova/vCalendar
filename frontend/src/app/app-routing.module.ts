68d9744300d192a4296be878cb445a93630d7fe6
Elena Kostyukova <Elena.Kostyukova@ingrammicro.com>
2020-02-03 09:28:04 +0700

    Basic routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { TableComponent } from './table/table.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'address-form', component: AddressFormComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
